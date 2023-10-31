9dff4fc5cde728f50acad558cb2afe9374ec7932
Andrei Mihai <mihaiandrei_zapuc@yahoo.com>
2023-10-31 21:35:15 +0200

    SmallUpdate2

--- firewall-rule.ps1
+++ firewall-rule.ps1
@@ -1,1 +1,2 @@
 New-NetFirewallRule -DisplayName "Allow Port 5000" -Direction Inbound -LocalPort 5000 -Protocol TCP -Action Allow
+#Firewall Rule to open port 5000
--- sysprep.ps1
+++ sysprep.ps1
@@ -4,3 +4,4 @@ Write-Host "Running Sysprep..."
 & "$Env:SystemRoot\System32\Sysprep\Sysprep.exe" /oobe /generalize /shutdown
 
 Write-Host "Sysprep complete."
+#Install sysprep to get the custom image.
